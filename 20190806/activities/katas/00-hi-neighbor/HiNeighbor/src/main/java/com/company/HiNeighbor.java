d49dae5b7f8bf0a2fc083d0e70637bf2c80a6c3d
terrenceturner <tsturner24@gmail.com>
2019-08-05 23:39:42 -0400

    Added HiNeighbor

---
+++ 20190806/activities/katas/00-hi-neighbor/HiNeighbor/src/main/java/com/company/HiNeighbor.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class HiNeighbor {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi Neighbor!");
+        System.out.println("My name is Maryam.");
+        System.out.println("I'm from Sandy Springs.");
+        System.out.println("I have 2 siblings.");
+        System.out.println("Pink is my favorite color.");
+        System.out.println("I listen to pop music.");
+        System.out.println("My favorite hobby is watching movies.");
+        System.out.println("Nice to meet you!");
+        System.out.println("Good bye!!!");
+    }
+}
