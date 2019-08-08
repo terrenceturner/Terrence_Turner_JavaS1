607e6eacbbe68e6bb306b4e63e6841ce1e42181b
terrenceturner <tsturner24@gmail.com>
2019-08-08 16:39:41 -0400

    completed katas

--- 20190807/activities/01-rectangular-paving-company/starter-code/RectPavingCompany/src/main/java/com/company/RectPavingCompany.java
+++ 20190807/activities/01-rectangular-paving-company/starter-code/RectPavingCompany/src/main/java/com/company/RectPavingCompany.java
@@ -6,7 +6,42 @@ import java.util.Scanner;
 
 public class RectPavingCompany {
 
+
+
+//    public static int promptDimension(String msg){
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.format("What is the %s of the driveway in  feet" , msg );
+//
+//        return Integer.parseInt(scan.nextLine()) ;
+//    }
+//
+//    public static void outputDimensions(String name , int value){
+//        System.out.format("The %s of the driveway is %d square feet.%n" , name , value);
+//    }
+//
+//
+//    public static void outputCosts(String name , float value) {
+//        System.out.format("The cost of the %s is %f . %n" , name , value );
+//    }
+
     public static void main(String[] args) {
+
+        int length = promptDimension("length");
+        int width = promptDimension("Width");
+        float cementCost = 12.5f;
+        float framingCost = 8.25f;
+
+        int area = length * width;
+        int perimeter = 2 * length + 2 * width;
+
+        outputDimensions("area", area);
+        outputDimensions("perimeter", perimeter);
+
+        outputCosts("Cement", area * cementCost);
+        outputCosts("framing/footers", perimeter * framingCost);
+    }
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter width of driveway: " + "ft");
