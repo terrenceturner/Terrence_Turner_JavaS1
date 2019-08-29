ba74ffe807f66672f22fe5c4bf50350ea7e2636c
terrenceturner <tsturner24@gmail.com>
2019-08-29 05:02:10 -0400

    hello web service

--- 20190829/hello-web-service/src/main/java/com/company/hellowebservice/controller/HelloController.java
+++ 20190829/hello-web-service/src/main/java/com/company/hellowebservice/controller/HelloController.java
@@ -2,11 +2,9 @@ package com.company.hellowebservice.controller;
 
 import com.company.hellowebservice.model.Hello;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class HelloController {
 
     //Get
@@ -14,7 +12,7 @@ public class HelloController {
 
     @ResponseStatus(value = HttpStatus.OK)
 
-    public Hello getGreeting(@PathVariable Hello greeting){
-        return greeting;
+    public Hello getGreeting(@PathVariable Hello name){
+        return name;
     }
 }
